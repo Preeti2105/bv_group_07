b9859545176f50953c650a0f104d2234a056a251
Preeti2105 <ps15068@gmail.com>
2022-04-20 20:48:53 +0530

    first commit

---
+++ src/pages/gallery.js
@@ -0,0 +1,182 @@
+import React from "react"
+import Layout from "../components/Layout/Layout"
+import Image from "../components/Images/Images"
+const gallery = () => {
+  return (
+    <Layout>
+      <div class="album py-5 bg-danger">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g1.jpg"
+            alt="1"
+          />
+            
+        </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g2.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g3.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g4.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g5.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g6.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g7.jpg"
+            alt="1"
+          />
+           
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g8.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g9.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g10.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g11.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g12.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g13.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g14.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g16.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <Image
+            className="d-block mx-lg-auto img-fluid h"
+            filename="g15.jpg"
+            alt="1"
+          />
+            
+          </div>
+        </div>
+        
+
+      </div>
+    </div>
+  </div>
+    </Layout>
+  )
+}
+export default gallery
+
